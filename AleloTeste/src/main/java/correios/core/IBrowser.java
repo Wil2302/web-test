f83cb98e4d0b29f69447bc1ac01bbd18097882f7
Velloso de Amorim Neto <wnvo@gft.com>
2020-03-26 15:51:16 -0300

    rename projet

--- AleloTeste/src/main/java/correios/core/IBrowser.java
+++
@@ -1,8 +0,0 @@
-package correios.core;
-
-import java.io.IOException;
-
-public interface IBrowser {
-
-	 public void setupBrowser() throws IOException;
-}
---
+++ AleloTeste/src/main/java/web/constants/Globals.java
@@ -0,0 +1,11 @@
+package web.constants; 
+
+import org.json.simple.JSONObject;
+
+public final class Globals {
+	
+	public static String PATH = "InfoCEP.json";
+	public static String TAG;
+	public static JSONObject DADOS_CEP;
+
+}
